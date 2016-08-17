d06c03211386b09f60c5376b10f25c91111b078b
Priyam Sarma <priyam.sarma@gmail.com>
2016-08-17 02:34:00 +0000

    Adds corrections to button variable and submits final solution

--- formValidation/main.js
+++ formValidation/main.js
@@ -1,31 +1,28 @@
 $(document).ready(function(){
+  var button = $('#submit');
+  button.on('click', function(){
+      var name = $('#name');
+      var email = $('#email');
+      var phone = $('#phone');
+      var required = [name,email,phone];
+  //    console.log(required);
 
-  var button = $('button');
+      for (var i = 0; i < required.length; i++){
+        // console.log("THis is where we are");
+        if (required[i].val() === ""){
+          var message = $('p#message');
+          message.html("Please Fill Out Required Fields");
+          message.addClass("warning");
+          required[i].prev('label').addClass("warning");
+        }else{
+          required[i].prev('label').removeClass("warning");
+        }
+      }
 
-  function validation(){
-    var name = $('#name');
-    var email = $('#email');
-    var phone = $('#phone');
-    var required = [name,email,phone];
-//    console.log(required);
-
-
-    for (var i = 0; i < required.length; i++){
-      // console.log("THis is where we are");
-      if (required[i].val() === ""){
-        var message = $('p#message');
-        message.html("Please Fill Out Required Fields");
-        message.addClass("warning");
-        required[i].prev('label').addClass("warning");
+      if ($('label.warning').length === 0){
+        $('form').remove();
+        $('h2').html("Thanks for your feedback!");
       }
     }
-
-    if ($('label.warning').length === 0){
-      $('.form-container').remove();
-      $('h2').html("Thanks for your feedback!");
-    }
-  }
-
-  button.on('click', validation(event));
-
+  )
 })
